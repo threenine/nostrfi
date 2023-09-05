e3f60f7ea67f515d4c5545c81f3917b3c85e8a70
Gary Woodfine <gary.woodfine@threenine.co.uk>
2023-09-05 19:55:31 +0100

    initial project config

---
+++ src/Infrastructure/Persistence/NostrfiDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using Database;
+using Microsoft.EntityFrameworkCore;
+using Threenine.Configurations.PostgreSql;
+
+namespace Nostrfi;
+
+public class NostrfiDbContext : DbContext
+{
+    public NostrfiDbContext(DbContextOptions<NostrfiDbContext> options) : base(options)
+    {
+    }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema(DefaultSchema.Name);
+        modelBuilder.HasPostgresExtension(PostgreExtensions.UUIDGenerator);
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(modelBuilder);
+    }
+
+}
---
+++ src/Infrastructure/Persistence/PersistenceConstants.cs
@@ -0,0 +1,6 @@
+namespace Database;
+
+internal static class DefaultSchema
+{
+    internal const string Name = "nostrfi";
+}
