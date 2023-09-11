bc1d1e5af281e1f94777cd4a7efdd82b6155ec58
Gary Woodfine <lnb0l9dc@duck.com>
2023-09-11 22:37:33 +0100

    Started WebSocket Implementation

--- src/Relay/Program.cs
+++ src/Relay/Program.cs
@@ -1,10 +1,9 @@
-using System.Net;
-using System.Net.WebSockets;
-using System.Text;
 using EFCoreSecondLevelCacheInterceptor;
 using Microsoft.EntityFrameworkCore;
 using Nostrfi;
 using Nostrfi.Extensions;
+using Nostrfi.Relay;
+using Nostrfi.Relay.Handlers;
 using Nostrfi.Relay.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -26,35 +25,17 @@ builder.Services.AddEFSecondLevelCache(options =>
     options.UseMemoryCacheProvider(CacheExpirationMode.Sliding, TimeSpan.FromMinutes(5)).DisableLogging(true).UseCacheKeyPrefix(Constants.CachePreFix));
 
 builder.Services.AddHostedService<MigrationsHostedService>();
+builder.Services.AddSingleton<WebSocketHandler>();
+builder.Services.AddSingleton<WebSocketMiddleWare>();
 
 builder.WebHost.UseUrls("http://localhost:5000");
 
 
 var app = builder.Build();
 
-using var serviceScope = app.Services.GetRequiredService<IServiceScopeFactory>().CreateScope();
-var context = serviceScope.ServiceProvider.GetService<NostrfiDbContext>();
-context?.Database.Migrate();
+
 app.UseWebSockets();
-app.Map("/ws", async context =>
-{
-    if(context.WebSockets.IsWebSocketRequest)
-    {
-        using var ws = await context.WebSockets.AcceptWebSocketAsync();
-        var message = "Hello World!";
-        var bytes = Encoding.UTF8.GetBytes(message);
-        var arraySegment = new ArraySegment<byte>(bytes, 0, bytes.Length);
-        await ws.SendAsync(arraySegment,
-            WebSocketMessageType.Text,
-            true,
-            CancellationToken.None);
-        
-    }
-    else
-    {
-        context.Response.StatusCode = (int) HttpStatusCode.BadRequest;
-    }
-});
+app.UseMiddleware<WebSocketMiddleWare>();
 
 
 app.Run();
